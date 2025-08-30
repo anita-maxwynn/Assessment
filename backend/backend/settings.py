f4967fd8a6d884a2c0b9fd913f2c268afe421356
anita-maxwynn <cse22100@gmail.com>
2025-08-30 23:59:50 +0530

    4th one

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%^%3q%6d-#sf$ytorjb_yut2l4awh$e(0c2xk=2(+32g3y0blx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ["assessment-twyo.onrender.com"]
 CORS_ALLOWED_ORIGINS = [
